f32f6232ce015687c879f931dca35a78587a456e
liu811585982 <811585982@qq.com>
2018-10-16 11:21:36 +0800

    add file

---
+++ src/pages/node/node.vue
@@ -0,0 +1,11 @@
+<template>
+  <page-container :breadbrumb="i18nBreadcrumb">
+    node list
+  </page-container>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style lang="less" scoped></style>
