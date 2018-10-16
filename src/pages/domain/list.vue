bbaa38c70c04bc5b4e0a12d0eb4abf140f738bf1
liu811585982 <811585982@qq.com>
2018-10-16 20:42:55 +0800

    折叠面板组件

--- src/pages/domain/list.vue
+++ src/pages/domain/list.vue
@@ -2,17 +2,22 @@
   <page-container :breadcrumb="i18nBreadcrumb">
     <!-- 工具条 -->
     <div class="toolbar" ref="toolbar">
-      <el-button type="iconButton" icon="h-icon-edit" @click="dialogVisible = true">修改虚拟IP</el-button>
+      <el-button type="iconButton" icon="h-icon-edit" @click="dialogVisible = !dialogVisible">修改虚拟IP</el-button>
     </div>
     <!-- 列表 -->
-    域名管理
+    <collapse-transition>
+      <div class="collapse-wraper" v-show="dialogVisible">
+        域名管理
+      </div>
+    </collapse-transition>
   </page-container>
 </template>
 <script>
   import pageTable from '@/components/pageTable'
+  import collapseTransition from '@/componentsFn/collapseTransition'
   export default {
     name: 'clusterList',
-    components: { pageTable },
+    components: {pageTable, collapseTransition},
     props: ['breadcrumbObj'],
     data () {
       return {
