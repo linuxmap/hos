808f66412d53e910413b0e6cbd773081142a4896
liu811585982 <811585982@qq.com>
2018-10-19 15:54:59 +0800

    云存储添加编辑表单验证

--- src/pages/cloud/add.vue
+++ src/pages/cloud/add.vue
@@ -1,8 +1,9 @@
 <template>
   <page-container :breadcrumb="i18nBreadcrumb">
+    <div class="basicMes">
     <el-form ref="cloudForm" label-width="120px" :model="cloudForm" :rules="cloudRules" content-width="500px">
       <el-form-item label="云ID" prop="cloud_id">
-        <el-input v-model="cloudForm.cloud_id"></el-input>
+        <el-input v-model="cloudForm.cloud_id" :minlength="1" :maxlength="64"></el-input>
       </el-form-item>
       <el-form-item label="云存储IP" prop="cloud_ip">
         <el-input v-model="cloudForm.cloud_ip"></el-input>
@@ -16,32 +17,72 @@
       <el-form-item label="用户名" prop="cloud_user">
         <el-input v-model="cloudForm.cloud_user"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="cloud_user">
-        <el-input v-model="cloudForm.cloud_user"></el-input>
+      <el-form-item label="密码" prop="cloud_password">
+        <el-input v-model="cloudForm.cloud_password" type="password"></el-input>
       </el-form-item>
       <el-form-item label="云存储访问key" prop="cloud_ak">
-        <el-input v-model="cloudForm.cloud_ak"></el-input>
+        <el-input v-model="cloudForm.cloud_ak" :minlength="3" :maxlength="64"></el-input>
       </el-form-item>
       <el-form-item label="协议加密key" prop="cloud_sk">
-        <el-input v-model="cloudForm.cloud_sk"></el-input>
+        <el-input v-model="cloudForm.cloud_sk" :minlength="3" :maxlength="64"></el-input>
+      </el-form-item>
+      <el-form-item label="是否本云" prop="type">
+        <el-radio-group v-model="cloudForm.type">
+          <el-radio label="1">是</el-radio>
+          <el-radio label="0">否</el-radio>
+        </el-radio-group>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handleSubmit('cloudForm')">设置</el-button>
+        <el-button type="primary" @click="handleSubmit('cloudForm')">提交</el-button>
         <el-button @click="resetForm('cloudForm')">重置</el-button>
       </el-form-item>
     </el-form>
+    </div>
   </page-container>
 </template>
 
 <script>
-  import validate from 'index@/utils/form-validate'
+  import validates from 'index@/utils/form-validate'
   export default {
     name: 'cloudAdd',
     props: ['breadcrumbObj'],
     data() {
       return {
-        cloudForm: {},
+        cloudForm: {
+          type: '0'
+        },
         cloudRules: {
+          cloud_id: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string1, trigger: 'blur change'}
+          ],
+          cloud_ip: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.ipv4, trigger: 'blur'}
+          ],
+          cloud_port: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.port, trigger: 'blur'}
+          ],
+          cloud_object_port: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.port, trigger: 'blur'}
+          ],
+          cloud_username: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string1, trigger: 'blur'}
+          ],
+          cloud_password: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'}
+          ],
+          cloud_ak: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string, trigger: 'blur'}
+          ],
+          cloud_sk: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string, trigger: 'blur'}
+          ]
         }
       }
     },
--- src/pages/cloud/edit.vue
+++ src/pages/cloud/edit.vue
@@ -1,5 +1,6 @@
 <template>
   <page-container :breadcrumb="i18nBreadcrumb">
+    <div class="basicMes">
     <el-form ref="cloudForm" label-width="120px" :model="cloudForm" :rules="cloudRules" content-width="500px">
       <el-form-item label="云ID" prop="cloud_id">
         <el-input v-model="cloudForm.cloud_id"></el-input>
@@ -16,8 +17,8 @@
       <el-form-item label="用户名" prop="cloud_user">
         <el-input v-model="cloudForm.cloud_user"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="cloud_user">
-        <el-input v-model="cloudForm.cloud_user"></el-input>
+      <el-form-item label="密码" prop="cloud_passwd">
+        <el-input v-model="cloudForm.cloud_passwd" type="password"></el-input>
       </el-form-item>
       <el-form-item label="云存储访问key" prop="cloud_ak">
         <el-input v-model="cloudForm.cloud_ak"></el-input>
@@ -30,11 +31,12 @@
         <el-button @click="resetForm('cloudForm')">重置</el-button>
       </el-form-item>
     </el-form>
+    </div>
   </page-container>
 </template>
 
 <script>
-  import validate from 'index@/utils/form-validate'
+  import validates from 'index@/utils/form-validate'
   import http from '@/libs/mockHttp'
   export default {
     name: 'cloudAdd',
@@ -43,6 +45,37 @@
       return {
         cloudForm: {},
         cloudRules: {
+          cloud_id: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string1, trigger: 'blur change'}
+          ],
+          cloud_ip: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.ipv4, trigger: 'blur'}
+          ],
+          cloud_port: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.port, trigger: 'blur'}
+          ],
+          cloud_object_port: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.port, trigger: 'blur'}
+          ],
+          cloud_username: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string1, trigger: 'blur'}
+          ],
+          cloud_password: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'}
+          ],
+          cloud_ak: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string, trigger: 'blur'}
+          ],
+          cloud_sk: [
+            {required: true, message: this.$t('config.validator.required'), trigger: 'blur'},
+            {validator: validates.utils_string, trigger: 'blur'}
+          ]
         }
       }
     },
