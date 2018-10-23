becdd51bee250304d77b68035d294690bc381cdb
liu811585982 <811585982@qq.com>
2018-10-23 17:12:57 +0800

    DNS服务集群页面

---
+++ src/pages/domain/domain.vue
@@ -0,0 +1,45 @@
+<template>
+  <page-card title="域名管理" :full="true">
+    <!-- 工具条 -->
+    <div class="toolbar" ref="toolbar">
+      <el-button type="iconButton" icon="h-icon-plus">添加域名</el-button>
+      <el-button type="iconButton" icon="h-icon-edit">添加域名</el-button>
+      <el-button type="iconButton" icon="h-icon-trashcan">添加域名</el-button>
+    </div>
+    <el-table
+      :data="tableData">
+      <el-table-column
+        type="selection">
+      </el-table-column>
+      <el-table-column
+        type="index">
+      </el-table-column>
+      <el-table-column
+        label="域名"
+        prop="domain_name">
+      </el-table-column>
+      <el-table-column
+        label="类型"
+        prop="domain_type">
+      </el-table-column>
+      <el-table-column
+        label="端口"
+        prop="port">
+      </el-table-column>
+      <el-table-column
+        label="IP列表"
+        prop="ip_list">
+      </el-table-column>
+    </el-table>
+  </page-card>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        tableData: []
+      }
+    }
+  }
+</script>
+<style lang="less" scoped></style>
---
+++ src/pages/domain/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <page-container :breadcrumb="i18nBreadcrumb">
+    <div class="page-left clearfix">
+      <dns-panel></dns-panel>
+    </div>
+    <div class="page-right clearfix">
+      <domain-panel></domain-panel>
+    </div>
+  </page-container>
+</template>
+<script>
+  import pageTable from '@/components/pageTable'
+  import dnsPanel from './dns'
+  import domainPanel from './domain'
+  export default {
+    name: 'clusterList',
+    props: ['breadcrumbObj'],
+    components: {dnsPanel, domainPanel},
+    data () {
+      return {
+      }
+    },
+    created () {
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .page-left, .page-right {
+    height: 100%;
+  }
+  .page-left {
+    float: left;
+    width: 40%;
+  }
+  .page-right {
+    float: right;
+    width: 60%;
+    padding: 0 12px 0 20px;
+  }
+</style>
